ab15dd881bef3ca810e268e8ab5f24ef43d10419
David Fusilier <dfusil2@gmail.com>
2016-05-15 16:59:47 -0400

    updated variables for round-under prop

--- config.rb
+++ config.rb
@@ -1,6 +1,7 @@
 require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 require 'sassy-math'
+require 'sane-scale'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
