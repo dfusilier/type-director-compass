e9ee78b34f4e3c096a0d338e2c7529f6a9b2777e
David Fusilier <dfusil2@gmail.com>
2016-07-30 12:02:05 -0400

    updated version numb

--- lib/sane-scale.rb
+++ lib/sane-scale.rb
@@ -17,8 +17,8 @@ Compass::Frameworks.register('sane-scale', :path => extension_path)
 # version contains alphas, it will be created as a 
 # prerelease version. Date is in the form of YYYY-MM-DD
 module SaneScale
-  VERSION = "0.6"
-  DATE = "2016-07-17"
+  VERSION = "0.7"
+  DATE = "2016-07-30"
 end
 
 # This is where any custom SassScript should be placed. The functions will be  
